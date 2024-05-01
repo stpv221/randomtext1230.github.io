26bb6ae8a37e1cb7f69594ba0ee9441b166ccc34
stpv221 <167645243+stpv221@users.noreply.github.com>
2024-05-01 19:14:40 +0000

    h

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const test = document.getElementById("url");
 
-test.innerHTML = window.location.href;
+test.textContent += window.location.href;
 
 if (window.location.href === "https://odin.is-probably.gay") {
   window.location.replace("https://stackoverflow.com");
