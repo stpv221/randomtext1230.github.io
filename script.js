a39c9c111061eb928de2826f352d7e1889dd711d
stpv221 <167645243+stpv221@users.noreply.github.com>
2024-05-02 16:47:10 +0000

    :DDDDDDDDDDDDDDDD

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 if (window.location.href === "https://odin.is-probably.gay") {
   window.location.replace("https://stackoverflow.com");
 }
+
+const fun = new Audio("/sound.mp3");
+
+for (let i = 0; i > fun.Length; i++){
+  fun.play();
+}
